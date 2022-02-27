0ab226bf6f9e73b43959ed192a2a42f8bb18c8a7
Peter Ade-Ojo <peteradeojo@outlook.com>
2022-02-27 09:07:32 +0100

    covid writeup

--- covid-19.php
+++ covid-19.php
@@ -6,26 +6,24 @@ use Utility\Functions;
 
 $navbar = null;
 const TITLE = 'COVID-19 Response';
+$levels = [['title' => 'COVID-19 Response']];
 
-Functions::extend('header');
+Functions::extend('inner-header');
 ?>
 
 <main id="main">
   <section>
-    <div class="container-fluid">
-
-      <h1 class="section-title text-dark">Our COVID experience</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus odio molestiae expedita minus nobis, numquam accusamus at aut repellat dolore, nisi illo quaerat aperiam error! Exercitationem, hic. Quod, nostrum aperiam.
-        Nam consectetur maiores similique odit distinctio, soluta sint suscipit, tempore quasi quia ut sapiente ullam aspernatur esse fugit voluptatibus consequatur dicta vero itaque animi minima accusamus perspiciatis! Esse, minima asperiores?
-        Magni iusto libero esse maxime est accusamus veritatis nesciunt at quidem enim molestiae numquam quos cum expedita molestias, doloremque nisi totam voluptates? Recusandae nisi, dolore cum quasi quas nemo. Adipisci!
-        Placeat praesentium ratione ab vel, harum quas accusantium illo at? Laborum omnis consectetur hic magni, harum vel necessitatibus doloremque tempore ratione temporibus rem delectus, aliquid labore ullam est a deserunt.
-        Repellat minima, voluptatem officia fugiat cumque ratione reprehenderit est, nulla veritatis ipsa corrupti ab possimus, nostrum dolorem architecto nobis! Harum delectus labore quae cumque optio ex odio, obcaecati facere aut.
-        Veritatis blanditiis quo perspiciatis minus itaque deleniti, optio vero reiciendis, impedit harum et tenetur molestiae laborum, officia adipisci provident. Aperiam, reiciendis? Sit et placeat laboriosam odit nostrum in cum quo.
-        Provident dignissimos minima exercitationem nobis dolorum sequi consectetur! Cupiditate ad esse totam nemo rem. Nam, laudantium ullam? Quos eaque deserunt nostrum iure, delectus facilis eligendi beatae? Quia, quisquam possimus! Necessitatibus!
-        Accusantium eos eaque optio illo, voluptatibus laudantium dolorem voluptate recusandae culpa explicabo pariatur fuga quae consequuntur? Vitae voluptatum, veritatis sapiente aliquid culpa cupiditate, voluptate perferendis ad atque libero rerum eveniet.
-        Aliquid temporibus doloremque perferendis laudantium, hic obcaecati facere dolore pariatur natus praesentium voluptatum nobis repudiandae et quas, dicta asperiores reiciendis modi tempore. Sapiente consequatur rem laborum saepe delectus nostrum dignissimos.
-        Aut quas beatae quisquam corrupti ducimus ipsa. Molestias laborum numquam laudantium consectetur doloremque reiciendis dolores voluptate ut! Dolorum, iure maxime, facere adipisci, tenetur cum et ex ipsum nemo sit ratione!</p>
+    <div class="container">
+      <h2 class="section-title">Our COVID Experience</h2>
+      <p>
+        When the world was rocked by the occurence of the coronavirus (COVID-19) pandemic, it became increasingly difficult to provide the interactive, hands-on care that we give to our patients, and among those who were affected greatly by this were our registered antenatal patients.
+      </p>
+      <p>However, the pandemic also brought about undeniable progress in the area of Telemedicine and Online Healthcare delivery by forcing healthcare providers to develop methods to consult with their patients remotely from the comfort of the patients' homes.</p>
+      <p>One of the ways this was shown in our hospital was the use of teleconferencing software to reach out to our patients and continually confer with and educate them about their health and best health practices, especially during the pandemic.</p>
     </div>
-
   </section>
 </main>
+
+<?php
+
+Functions::extend('footer');
